0269e9c2ba1d916cbe5cf1717e7db5ef59e7849b
evgeniia lemanova <jane87.05@mail.ru>
2023-03-11 19:13:27 +0300

    add classes and tests

--- src/stack.py
+++ src/stack.py
@@ -1,13 +1,14 @@
 class Node:
     """Класс для узла стека"""
 
-    def __init__(self, data, next_node):
+    def __init__(self, data, next_node=None):
         """
         Конструктор класса Node
 
         :param data: данные, которые будут храниться в узле
         """
-        pass
+        self.data = data
+        self.next_node = next_node
 
 
 class Stack:
@@ -15,7 +16,8 @@ class Stack:
 
     def __init__(self):
         """Конструктор класса Stack"""
-        pass
+        self.top = None
+        self.stack = []
 
     def push(self, data):
         """
@@ -23,7 +25,11 @@ class Stack:
 
         :param data: данные, которые будут добавлены на вершину стека
         """
-        pass
+        new_node = Node(data)
+        self.stack.append(data)
+        if self.top is not None:
+            new_node.next_node = self.top
+        self.top = new_node
 
     def pop(self):
         """
@@ -31,4 +37,6 @@ class Stack:
 
         :return: данные удаленного элемента
         """
-        pass
+        result = self.top.data
+        self.top = self.top.next_node
+        return result
--- tests/test_stack.py
+++ tests/test_stack.py
@@ -1,1 +1,18 @@
 """Здесь надо написать тесты с использованием unittest для модуля stack."""
+import unittest
+from src.stack import Node, Stack
+
+class TestStack(unittest.TestCase):
+    def test_Node(self):
+        n1 = Node(1, 2)
+        self.assertEqual(n1.data, 1)
+        self.assertEqual(n1.next_node, 2)
+    def test_Stack(self):
+        stack = Stack()
+        stack.push('555')
+        stack.push('666')
+        self.assertEqual(stack.top.next_node.data, '555')
+        stack.pop()
+        self.assertEqual(stack.top.data, '555')
+
+
